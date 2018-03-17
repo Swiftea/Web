985a52058f9e867915f393a5f5e410df9a7b71c1
Hugo Posnic <hugo.posnic@gmail.com>
2018-03-17 23:55:56 +0100

    Initial design for search results

--- search.php
+++ search.php
@@ -7,4 +7,17 @@
     </form>
 </section>
 
+<section id="results">
+    <div class="result">
+        <p class="result-title"><img src="http://www.google.com/s2/favicons?domain=lemonde.fr" alt=""> <a href="#">Le Monde.fr - Actualités et Infos en France et dans le monde</a></p>
+        <p class="result-description">Le Monde.fr - 1er site d'information. Les articles du journal et toute l'actualité en continu : International, France, Société, Economie, Culture, Environnement, Blogs ...</p>
+        <p class="result-url">www.lemonde.fr</p>
+    </div>
+    <div class="result">
+        <p class="result-title"><img src="http://www.google.com/s2/favicons?domain=agence-sence.fr/" alt=""> <a href="#">Sence • Agence web à Orléans : création de site web et design</a></p>
+        <p class="result-description">Agence web jeune et créative, Sence développe avec vous votre site web, votre identité graphique et vous accompagne dans votre stratégie numérique</p>
+        <p class="result-url">agence-sence.fr</p>
+    </div>
+</section>
+
 <?php require_once('templates/footer.php'); ?>

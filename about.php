2cfb1e44e6ce4af4d1796d8da557cd5535a2f470
Hugo Posnic <hugo.posnic@gmail.com>
2018-03-19 18:45:30 +0100

    Change a link

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 
 <section class="page">
     <h1>À propos</h1>
-    <p>Swiftea est un moteur de recherche open-source développé par <a href="https://thykof.github.io">Nathan Seva</a> et <a href="https://hugo-posnic.fr">Hugo Posnic</a> dans un but éducatif.</p>
+    <p>Swiftea est un moteur de recherche open-source développé par <a href="https://github.com/Thykof">Nathan Seva</a> et <a href="https://hugo-posnic.fr">Hugo Posnic</a> dans un but éducatif.</p>
 
     <h2>Open Source</h2>
     <p>Le projet est <a href="https://github.com/Swiftea">disponible sur GitHub</a> sous licence GNU GPL v3.</p>

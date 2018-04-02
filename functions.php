5381b4f7f3150e2a6d7fcd353b2a78a5eb560696
Hugo Posnic <hugo.posnic@gmail.com>
2018-04-02 16:52:43 +0200

    First try to create a cache system

--- functions.php
+++ functions.php
@@ -1,9 +1,41 @@
 <?php
+// Get a value in cache by its name
+function get_cache_value($name) {
+    $json_data = file_get_contents('cache/values.json');
+    $array = json_decode($json_data, true);
+
+    // Verify duration
+    $seconds = 60 * 60 * $array[$name]['duration'];
+    if((time() - $array[$name]['time']) < $seconds) {
+        return $array[$name]['value'];
+    }
+    else {
+        return NULL;
+    }
+}
+
+// Save a value in cache with its name and a duration
+function set_cache_value($name, $value, $hours=2) {
+    $array = array(
+        $name => array(
+            'value' => $value,
+            'duration' => $hours,
+            'time' => time()
+        )
+    );
+    file_put_contents('cache/values.json', json_encode($array));
+}
+
 function get_index_size($db) {
-    $sql = 'SELECT count(*) FROM website';
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    return $stmt->fetchColumn();
+    $index_size = get_cache_value('index_size');
+    if (!$index_size) {
+        $sql = 'SELECT count(*) FROM website';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $index_size = $stmt->fetchColumn();
+        set_cache_value('index_size', $index_size);
+    }
+    return $index_size;
 }
 
 function get_base_url() {
@@ -19,13 +51,13 @@ function get_base_url() {
 }
 
 function truncate_str($str, $length, $beautiful = true) {
-    //no need to trim, already shorter than trim length
+    // no need to trim, already shorter than trim length
     if (strlen($str) <= $length) {
         return $str;
     }
 
     if ($beautiful) {
-        //find last space within length
+        // find last space within length
         $last_space = strrpos(substr($str, 0, $length), ' ');
         $trimmed_text = substr($str, 0, $last_space);
     }

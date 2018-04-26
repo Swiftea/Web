a5d7a7d118bebac018e8e95b99c47a1f75bcf9f4
Hugo Posnic <hugo.posnic@gmail.com>
2018-04-26 15:43:48 +0200

    Fix a conflict between same vars that prevent access to search pages

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once('config.php');
 require_once('functions.php');
 
 // Get page
-if(isset($_GET['p']) && !empty($_GET['p'])) {
-    $tmp_page = htmlspecialchars($_GET['p']);
+if(isset($_GET['page']) && !empty($_GET['page'])) {
+    $tmp_page = htmlspecialchars($_GET['page']);
     if(file_exists('pages/' . $tmp_page . '.php')) {
         $page = $tmp_page;
     }

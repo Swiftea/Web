3a3aa616db0e367116d16628bb87e4742aaea6aa
swifteasearch <thykof@protonmail.ch>
2019-08-14 21:03:42 +0200

    Fix search: inverted index path

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ require_once('db.php');
 // Vars
 
 $languages = array('en', 'fr');
-$index_folder = 'data/inverted_index/';
+$index_folder = '../data/inverted_index/';
 $index_ext = '.sif';
 $min_length_keyword = 2;
 $max_results_per_page = 10;

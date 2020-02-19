df7382ec0291d1f52a9b6f84e6f3d4ad4510068e
Hugo Posnic <hposnic@altais.fr>
2020-02-19 22:25:06 +0100

    Change description

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@
         }
         echo '<title>' . $title . 'Swiftea</title>';
         ?>
-        <meta name="description" content="Get data on many websites for your needs by using our free and open-source database of thousands of websites with the help of our API.">
+        <meta name="description" content="Drive research on your website with our powerful & deep analysis tool!">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="assets/css/master.min.css">
         <link rel="stylesheet" href="assets/css/prism.css">

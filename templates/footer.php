7bc4b23bce4adb4ceda04eae0792131c85ef98ce
Hugo Posnic <hposnic@altais.fr>
2020-02-19 22:31:32 +0100

    Remove API

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,6 @@
     </div>
     <footer id="footer">
         <ul>
-            <li class="featured"><a href="use-api"><i class="fas fa-cogs"></i> API</a></li>
             <li><a href="status">Status</a></li>
             <li><a href="about">About</a></li>
         </ul>

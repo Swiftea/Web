4380c937b02244f491d4ffd891ca0f6c2451a2d0
Hugo Posnic <hposnic@altais.fr>
2020-02-23 17:17:17 +0100

    Remove prism

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,9 @@
-    </div>
-    <footer id="footer">
-        <ul>
-            <li><a href="status"><i class="fas fa-cogs"></i> Status</a></li>
-            <li><a href="about"><i class="fas fa-info-circle"></i> About</a></li>
-        </ul>
-    </footer>
-    <script src="assets/js/prism.js"></script>
+        </div>
+        <footer id="footer">
+            <ul>
+                <li><a href="status"><i class="fas fa-cogs"></i> Status</a></li>
+                <li><a href="about"><i class="fas fa-info-circle"></i> About</a></li>
+            </ul>
+        </footer>
     </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -14,7 +14,6 @@
         <meta name="description" content="Drive research on your website with our powerful & deep analysis tool!">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="assets/css/master.min.css">
-        <link rel="stylesheet" href="assets/css/prism.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <script src="https://kit.fontawesome.com/95303e0f2f.js"></script>
         <link rel="icon" type="image/png" href="assets/img/icon-256x256-dark-round.png">

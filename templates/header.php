8d6ff208f9b487d6134acd14c9c8912f34b2c75b
Hugo Posnic <hposnic@altais.fr>
2020-02-23 20:07:10 +0100

    Update titles

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@
         }
         echo '<title>' . $title . 'Swiftea</title>';
         ?>
-        <meta name="description" content="Drive research on your website with our powerful & deep analysis tool!">
+        <meta name="description" content="Drive research of your website with our powerful & deep analysis tool!">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="assets/css/master.min.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">

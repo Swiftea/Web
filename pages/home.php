211fa8a1400375e50504752fb6aab1ae1c7137c5
Hugo Posnic <hposnic@altais.fr>
2020-02-23 16:01:08 +0100

    Some design adjustments

--- pages/home.php
+++ pages/home.php
@@ -7,9 +7,9 @@ require_once('templates/header.php');
     <a href="<?php echo get_base_url(); ?>" id="logo"><img src="assets/img/logo-241x75.png" alt="Swiftea"></a>
 
 	<form method="GET" action="internal-search-result">
-        <input type="search" name="q" placeholder="Your search..." autocomplete="off" autofocus>
-        <input type="text" name="d" placeholder="Website url" autocomplete="off" autofocus>
-        <button type="submit" class="btn"><i class="fas fa-search"></i> Search in a website</button>
+        <input type="search" name="q" placeholder="Your search" autocomplete="off" autofocus>
+        <input type="text" name="d" placeholder="The URL of the site you want to browse" autocomplete="off" autofocus>
+        <button type="submit" class="btn"><i class="fas fa-search"></i> Search in this website</button>
     </form>
 
 </section>
--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,8 @@
     </div>
     <footer id="footer">
         <ul>
-            <li><a href="status">Status</a></li>
-            <li><a href="about">About</a></li>
+            <li><a href="status"><i class="fas fa-cogs"></i> Status</a></li>
+            <li><a href="about"><i class="fas fa-info-circle"></i> About</a></li>
         </ul>
     </footer>
     <script src="assets/js/prism.js"></script>
